142ab062b1cdb7048efb7e60aff4ed5ba7e30b64
Kgiruc <kgiruc76@gmail.com>
2022-08-31 14:16:39 +0200

    scss

--- portfolio/src/components/FixBlock.jsx
+++ portfolio/src/components/FixBlock.jsx
@@ -1,6 +1,6 @@
 function FixBlock() {
     return (
-        <div>
+        <div className="fixblock__container">
             <h1>Cześć. Jestem software developerem.</h1>
             <span>Mam na imię Karol Giruć. Uwielbiam tworzyć nowe i ciekawe rzeczy.</span>
             <button> Zacznijmy pracować razem!</button>
