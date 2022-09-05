c8ddab7738a7fecd8cc43ac48456e03789da7113
Kgiruc <kgiruc76@gmail.com>
2022-09-05 14:17:01 +0200

    scss

--- portfolio/src/components/FixBlock.jsx
+++ portfolio/src/components/FixBlock.jsx
@@ -1,9 +1,13 @@
 function FixBlock() {
     return (
         <div className="fixblock__container">
-            <h1>Cześć. Jestem software developerem.</h1>
+            <h1>Cześć. Jestem frontend developerem.</h1>
             <span>Mam na imię Karol Giruć. Uwielbiam tworzyć nowe i ciekawe rzeczy.</span>
             <button> Zacznijmy pracować razem!</button>
+            <footer>
+                <a href="https://github.com/Kgiruc" target="_blank">Github</a> and
+                <a href="https://www.linkedin.com/in/karol-giruc/" target="_blank"> LinkedIn</a>
+            </footer>
         </div>
     );
 }
