20b5ec4d1412ca7d01eb7b25900862c0b87cfd2c
Kgiruc <kgiruc76@gmail.com>
2022-09-07 14:42:11 +0200

    article home

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -2,12 +2,20 @@ function Home() {
     return (
         <div className="home__container">
               <main>
-                  <header>O mnie</header>
+                  <h1>O mnie</h1>
                   <article className="home main__article">
                       Lubię pracować blisko narzuconego projektu i opracowywać go co do pixela.
                       Wiele radości sprawia mi uczenie się nowych rzeczy i podążanie wraz z nowymi technologiami.
                       Codziennie staram się podpatrywać sposób pisania kodu developerów i wprowadzać je w swoje projekty.
                   </article>
+                  <h2>Techniczne</h2>
+                  <section>
+
+                  </section>
+                  <h2>Personalne</h2>
+                  <section>
+
+                  </section>
               </main>
         </div>
     );
