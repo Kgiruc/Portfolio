151150040ae5b3a5180dedc1d3a7ad69eb73c834
Kgiruc <kgiruc76@gmail.com>
2022-09-10 15:47:15 +0200

    article home

--- portfolio/src/components/Experience.jsx
+++ portfolio/src/components/Experience.jsx
@@ -1,7 +1,15 @@
+import {useState} from "react";
+
 function Experience() {
+    const [isHover, setIsHover] = useState(false);
+
     return (
         <article className="experience__container">
-        <span>Surf Burger</span>
+        <span
+            onMouseEnter={() => setIsHover(true)}
+            onMouseLeave={() => setIsHover(false)}>
+            {isHover ? "nope"  : "SurfBurger" }
+        </span>
         <span>Teleakcess Galeria Morena</span>
         <span>Global GSM</span>
         </article>
