e62685e58f249c38f20dca9b0dfcf93752c597b4
Kgiruc <kgiruc76@gmail.com>
2022-09-30 01:05:12 +0200

    buttons project filter

--- portfolio/src/components/Experience.jsx
+++ portfolio/src/components/Experience.jsx
@@ -1,4 +1,7 @@
 import {useState} from "react";
+import burger from "../assets/icons/burger.png";
+import smartphone from "../assets/icons/smartphone.png";
+import telephone from "../assets/icons/telephone.png";
 
 function Experience() {
     const [isHover, setIsHover] = useState(false);
@@ -11,17 +14,17 @@ function Experience() {
             onMouseEnter={() => setIsHover(true)}
             onMouseLeave={() => setIsHover(false)}>
             {isHover ? "Składanie burgerów, praca w większej grupie, współpraca, odpowiedzialność za powierzone " +
-                "zadania "  : "SurfBurger" }
+                "zadania "  : "SurfBurger " }<img src={burger} alt='burger' className='icons'/>
         </span><span
             onMouseEnter={() => setIsHover2(true)}
             onMouseLeave={() => setIsHover2(false)}>
             {isHover2 ? "Naprawiane telefonów, sprzedaż akcesoriów, praca z oprogramowaniem smartfonów"
-                : "Teleakces" }
+                : "Teleakces" }<img src={smartphone} alt='smartphone' className='icons'/>
         </span><span
             onMouseEnter={() => setIsHover3(true)}
             onMouseLeave={() => setIsHover3(false)}>
             {isHover3 ? "Naprawianie telefonów, dokonywanie zmian w oprogramowaniu smartfonów"
-                : "Global GSM" }
+                : "Global GSM" }<img src={telephone} alt='telephone' className='icons'/>
         </span>
         </article>
     );
