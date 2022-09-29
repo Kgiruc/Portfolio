1b47af35b0b21a3d25560f5c3230a68045efa550
Kgiruc <kgiruc76@gmail.com>
2022-09-29 15:06:00 +0200

    buttons project

--- portfolio/src/components/Projects/Projects-Menu.jsx
+++ portfolio/src/components/Projects/Projects-Menu.jsx
@@ -1,5 +1,7 @@
 import {useState} from "react";
 import ProjectsAll from "./ProjectsList.jsx";
+import monitor from "../../assets/icons/monitor.png";
+import github_logo from "../../assets/icons/github.png";
 
 function ProjectsMenu() {
     const [state, setState] =useState(ProjectsAll);
@@ -8,13 +10,13 @@ function ProjectsMenu() {
             {
                 state.map((item) => {
                     return <section className="projects__menu__container" key={item.id}>
-                        <img className="projects__menu__img" src={item.img} alt="planet" />
+                        <img className="projects__menu__img" src={item.img} alt="screenshot" />
                         <article className="projects__menu__subs">
                             <h2>{item.title}</h2>
                             <p>{item.body}</p>
                             <div className="projects__results">
-                                <span>{item.demo}</span>
-                                <span>{item.github}</span>
+                                <a href={item.demo} _blank><img src={monitor} alt="demo"/></a>
+                                <a href={item.github}><img src={github_logo} alt="github"/></a>
                             </div>
                         </article>
                     </section>
