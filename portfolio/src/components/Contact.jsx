40b91db0fa680ccccb003702a157620b3059eb96
Kgiruc <kgiruc76@gmail.com>
2022-10-03 16:20:45 +0200

    form css

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -3,34 +3,40 @@ import {useState} from "react";
 
 function Contact() {
     const [message, setMessage] = useState('');
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
 
     const formId = 'oRG4pJqJ';
     const formSparkUrl = `https://submit-form.com/${formId}`;
     const submitForm = async (e) => {
         e.preventDefault();
-        await fetch(formSparkUrl,{
+        await fetch(formSparkUrl, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
                 Accept: "application/json",
             },
             body: JSON.stringify({
+                name,
+                email,
                 message,
             }),
         });
-        alert("Form submitted");
+        alert("Wysłano pomyślnie");
     };
 
     return (
         <section className='contact__container'>
             <h4>
-                <img src={send} alt='paper plane icon' className='icons'/>
+                <img src={send} alt='paper plane icon' className='contact__icon icons'/>
                 Kontakt ze mną
             </h4>
-            <form onSubmit={submitForm}>
-                <textarea value={message} onChange={(e) => setMessage(e.target.value)} />
+            <form onSubmit={submitForm} className='contact__form__container'>
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <textarea value={message} onChange={(e) => setMessage(e.target.value)}/>
                 <button type='submit'>
-                    Submit
+                    >>
                 </button>
             </form>
         </section>
