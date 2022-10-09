e5589a9ab9e00ff25a9c3d88e84f9f49794453b9
Karol Giruc <kgiruc76@gmail.com>
2022-10-09 02:09:52 +0200

    three materials

--- portfolio/src/components/background/Elements.jsx
+++ portfolio/src/components/background/Elements.jsx
@@ -1,11 +1,9 @@
-
-
 function Elements() {
     return (
         <>
             <ambientLight intensity={1}/>
             <pointLight color="blue" position={[2, 0, 5]} intesity={2}/>
-            <mesh  position={[2, -2, -2]}>
+            <mesh  position={[2, -2, -2]} >
                 <boxGeometry args={[.5, .5, .5]}/>
                 <meshPhongMaterial color="green"/>
             </mesh>
